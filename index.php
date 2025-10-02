6cdac0779458d14725ee685f3a707873046f7564
rovenz <93180052+rovenz@users.noreply.github.com>
2025-10-02 16:21:18 +0800

    feat: Update index.php add the sweet alert plugins script

--- index.php
+++ index.php
@@ -84,5 +84,6 @@
                 integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
                 crossorigin="anonymous"></script>
         <script src="script.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     </body>
 </html>
